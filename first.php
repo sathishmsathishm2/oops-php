508c4868f354eb91895f8e5a8b273248f4ee258d
sathish <sathishmsathishm2@gmail.com>
2020-03-05 13:03:56 +0530

    Create first.php

---
+++ first.php
@@ -0,0 +1,21 @@
+<?php
+class fruits {
+    public $name;
+    public $color;
+    function set_name($name){
+        $this->name = $name;
+    }
+    function get_name(){
+        return $this->name;
+    
+
+    }
+
+}
+
+
+
+
+
+
+?>
